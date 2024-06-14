8f89f6d24bc1600356428d70f5708b38d6fbf7dd
Ahmed-3699 <ahmadawanmuhammad01@gmail.com>
2024-06-14 19:54:28 +0500

    configration

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(cors())
 app.use(express.json())
 app.use("/", router)
 
+app.get("/", (req, res) => {
+    res.send("Server Running Successfull!")
+})
+
 mongoose.connect("mongodb+srv://ahmed:db3699@cluster0.64w6f0j.mongodb.net/Create-Post").
 then(()=>{
     console.log("Db running");
@@ -18,6 +22,6 @@ then(()=>{
 
 
 
-app.listen(2000, ()=>{
+app.listen(3000, ()=>{
     console.log("Server Running");
 })
